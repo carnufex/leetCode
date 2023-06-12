a14cf5c1d040d64463e786b1e8ccb1625d68e894
Christopher Rosenvall <christopher.rosenvall@dataductus.se>
2023-06-12 12:52:59 +0200

    problem 11

--- leetCode/11. Container with most water/Solution11.cs
+++ leetCode/11. Container with most water/Solution11.cs
@@ -8,6 +8,28 @@ namespace leetCode._11._Container_with_most_water
 {
 	public class Solution11
 	{
+		public int MaxArea(int[] height)
+		{
+			int area = 0;
+			int left = 0;
+			int right = height.Length - 1;
+			while (left < right)
+			{
+				int currentArea = Math.Min(height[left], height[right]) * (right-left);
+				area = Math.Max(area, currentArea);
 
+				if (height[left] < height[right])
+				{
+					left += 1;
+				}
+				else
+				{
+					right -= 1;
+				}
+			}
+
+			return area;
+		}
+		
 	}
 }
